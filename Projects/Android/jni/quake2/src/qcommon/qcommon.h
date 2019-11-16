5f58850ba4a75947222832d0981835725e9bf2ee
Simon <simonbrown77@googlemail.com>
2019-11-16 14:19:36 +0000

    Updated version for beta 0.3

--- Projects/Android/jni/quake2/src/qcommon/qcommon.h
+++ Projects/Android/jni/quake2/src/qcommon/qcommon.h
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
 //#define	VERSION		3.21
-#define	VERSION		0.2
+#define	VERSION		0.3
 
 #define	BASEDIRNAME	"baseq2"
 
