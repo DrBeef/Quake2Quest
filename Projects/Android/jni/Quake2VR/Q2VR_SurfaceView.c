3b35b7db5ac79edc2cf2a2aaa72380fdddbcea32
Simon <simonbrown77@googlemail.com>
2020-12-10 20:46:24 +0000

    90hz Mode support for Quest 2

--- Projects/Android/jni/Quake2VR/Q2VR_SurfaceView.c
+++ Projects/Android/jni/Quake2VR/Q2VR_SurfaceView.c
@@ -1432,7 +1432,12 @@ void * AppThreadFunction( void * parm )
 
 	chdir("/sdcard");
 
-	for ( bool destroyed = false; destroyed == false; )
+    //Set 90hz mode for Quest 2
+    if (vrapi_GetSystemPropertyInt(&java, VRAPI_SYS_PROP_DEVICE_TYPE) == VRAPI_DEVICE_TYPE_OCULUSQUEST2) {
+        vrapi_SetDisplayRefreshRate(appState.Ovr, 90);
+    }
+
+    for ( bool destroyed = false; destroyed == false; )
 	{
 		for ( ; ; )
 		{
