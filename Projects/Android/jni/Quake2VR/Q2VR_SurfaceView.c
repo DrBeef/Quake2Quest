f39d4ebde290775744d4f9b2b402c69eaf6b1a77
Simon <simonbrown77@googlemail.com>
2020-12-12 11:48:12 +0000

    Ensure Quest 2 STAYS at 90hz
    VrApi was unnecessarily reducing it to 72hz!

--- Projects/Android/jni/Quake2VR/Q2VR_SurfaceView.c
+++ Projects/Android/jni/Quake2VR/Q2VR_SurfaceView.c
@@ -1432,11 +1432,6 @@ void * AppThreadFunction( void * parm )
 
 	chdir("/sdcard");
 
-    //Set 90hz mode for Quest 2
-    if (vrapi_GetSystemPropertyInt(&java, VRAPI_SYS_PROP_DEVICE_TYPE) == VRAPI_DEVICE_TYPE_OCULUSQUEST2) {
-        vrapi_SetDisplayRefreshRate(appState.Ovr, 90);
-    }
-
     for ( bool destroyed = false; destroyed == false; )
 	{
 		for ( ; ; )
@@ -1591,6 +1586,11 @@ void * AppThreadFunction( void * parm )
                 //runStatus = 0;
             }
 #endif
+
+            //Set 90hz mode for Quest 2
+            if (vrapi_GetSystemPropertyInt(&java, VRAPI_SYS_PROP_DEVICE_TYPE) == VRAPI_DEVICE_TYPE_OCULUSQUEST2) {
+                vrapi_SetDisplayRefreshRate(appState.Ovr, 90);
+            }
 
 			// Get the HMD pose, predicted for the middle of the time period during which
 			// the new eye images will be displayed. The number of frames predicted ahead
